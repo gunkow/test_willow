1c5738ad3633e73f1482688deb005a4249f3a6bf
gunkow <gunkow@gmail.com>
2016-09-12 12:57:15 +0300

    all GET methods

--- api/api.py
+++ api/api.py
@@ -10,3 +10,22 @@ def all():
     wikis = Wikipage.query.all()
     return jsonify(map(lambda wiki: wiki.asdict(), wikis))
 
+@main.route('/<int:wiki_id>')
+def versions(wiki_id):
+    wiki = Wikipage.query.get(wiki_id)
+    pages = wiki.pages
+    return jsonify(map(lambda page: page.asdict(), pages))
+
+@main.route('/<int:page_id>')
+def versions(page_id):
+    page = Page.query.get(page_id)
+    return jsonify(page.asdict())
+
+@main.route('/<int:wiki_id>/current')
+def versions(wiki_id):
+    wiki = Wikipage.query.get(wiki_id)
+    current_page = wiki.current_page
+    return jsonify(current_page.asdict())
+
+
+
